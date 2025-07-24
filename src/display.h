6dde6ae6dfac0cf9f7811c0187b85736deb1a726
ghostxmerlin <ghostxmerlin@gmail.com>
2025-07-24 16:52:25 +0800

    parse psbt to get output

--- src/display.h
+++ src/display.h
@@ -23,5 +23,5 @@
 
 bool display_transaction(dispatcher_context_t *dc,
                          int64_t value_spent,
-                         uint64_t magic_input_value,
+                         uint8_t* scriptpubkey,
                          uint64_t fee);
